d0785331e13d0391c9e40f2d429b4a036f1c4acf
Yurick-coinS <88509970+Yurick-coinS@users.noreply.github.com>
2025-07-20 03:44:52 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import './App.css'; // Подключаем стили
+
+function App() {
+  // Пока что наш компонент просто возвращает HTML-разметку
+  return (
+    <div className="app">
+      <h1>DEGEN-MAN</h1>
+      <p className="subtitle">The Maze of F.U.D.</p>
+    </div>
+  );
+}
+
+export default App;
